6b75a6cdc026d00c6c1a9d97585285a0b99df3aa
Andre Simao <asimao@me.com>
2019-10-09 11:58:00 -0300

    Restaurant in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chez = Restaurant.create(name: "Chez", address: "123 T St", category: "chinese")
+john = Restaurant.create(name: "John", address: "456 T St", category: "chinese")
+luca = Restaurant.create(name: "Luca", address: "789 T St", category: "chinese")
+marco = Restaurant.create(name: "Marco", address: "000 T St", category: "chinese")
+joao = Restaurant.create(name: "Joao", address: "12 T St", category: "chinese")
