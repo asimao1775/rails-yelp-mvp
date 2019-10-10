6b8c6d288f3873174af78e301aed64bfc74f0cb6
Andre Simao <asimao@me.com>
2019-10-10 20:50:20 -0300

    Yelp Ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:new, :index, :show, :create] do
-      resources :reviews, only: [:new, :index, :show, :create]
+      resources :reviews, only: [:new, :create]
     end
+
+  root to: "restaurants#index"
 end
